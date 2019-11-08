73cbd5570657dd34d0a134adc6129a5ec217da98
Yogendra Singh <yogsingh6@publicisgroupe.net>
2019-11-08 22:56:11 +0530

    Added Column Widget with Text and RaisedButton Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,64 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
-  runApp(MyApp());
+// void main(List<String> args) {
+//   runApp(MyApp());
+// }
+// or
+// For only expression we can use the arrow style
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      if (questionIndex < 1) {
+         questionIndex = questionIndex + 1;
+      }
+    });
+    print(questionIndex);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("Hello!"),);
+    var questions = [
+      "What's your favorite Color?",
+      "What is favorite Cricket Player?"
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text("answer 1"),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("answer 2"),
+              onPressed: () {
+                print("Answer 2 is Pressed");
+              },
+            ),
+            RaisedButton(
+              child: Text("answer 3"),
+              onPressed: () => print("Answer 3 is pressed"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
-
-
-
 }
